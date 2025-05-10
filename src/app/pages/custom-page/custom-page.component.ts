ac8e366167923c4ce49bc0d108cd7d51491bae31
Emerson Roncancio <roncancioavila@gmail.com>
2025-05-09 19:07:25 -0500

    Añadir nuevos pipes y ajustar la estructura del componente de la página personalizada

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,12 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { CardComponent } from "../../components/card/card.component";
+import { JsonPipe, KeyValuePipe, SlicePipe } from '@angular/common';
+
+const names = [
+  'Alice Smith',
+  'Bob Johnson',
+  'Charlie Brown',
+  'Diana Prince',
+  'Ethan Hunt'
+];
 
 @Component({
   selector: 'custom-page',
-  imports: [CardComponent],
+  imports: [CardComponent, SlicePipe, JsonPipe, KeyValuePipe],
   templateUrl: './custom-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CustomPageComponent {
 
+  users = signal(names);
+
+ user = {
+    name: 'Emerson',
+    age: 25,
+    email: 'emerson@gmail.com'
+ }
  }
